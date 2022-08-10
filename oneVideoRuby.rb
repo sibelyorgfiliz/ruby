4254d22c3c310f36bfbf8f2f29b387abe7977d4e
Syf <sibel.y.filiz@gmail.com>
2022-08-10 23:42:49 +0200

    commands, print, puts

---
+++ oneVideoRuby.rb
@@ -0,0 +1,18 @@
+
+#comment één lijn
+=begin
+meer dan één lijn
+we gebruiken 'begin- end' met '='.
+=end
+
+print "Hallo vrinden"
+print "Hallo vrinden\n"
+print "Hi friends!"
+
+puts("geen \\n nodig hier")  #1
+puts("een andere lijn")  #2
+print("burasi alt satira gececek, cunku onceki puts komutu \\n iceriyormus gibi davraniyor.") #3
+puts("bu satir alta inmeden ustteki print komutunun devami olarak ekrana yazacak.")
+#print--> \n is nodig
+# puts--> \n is niet nodig.
+
